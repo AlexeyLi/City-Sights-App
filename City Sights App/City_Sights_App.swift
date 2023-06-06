df7a1aaa041feac6afd9401f803fe1172cf2838c
alexeyli <alexeyli.sdet@gmail.com>
2023-06-06 12:55:25 -0700

    Set up folders for Views, Models and ViewModels

---
+++ City Sights App/City_Sights_App.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by Alexey Li on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_App: App {
+    var body: some Scene {
+        WindowGroup {
+            LaunchView()
+        }
+    }
+}
---
+++ City Sights App/Views/LaunchView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  City Sights App
+//
+//  Created by Alexey Li on 6/6/23.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
